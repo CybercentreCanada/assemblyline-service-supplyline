860d447fb32ce7470bc95d3c4960013bcd858277
cccs-jaw <244584135+cccs-jaw@users.noreply.github.com>
2026-05-06 20:03:24 +0000

    Is the AL harness causing the issue?

--- tests/test_supplyline_samples.py
+++ tests/test_supplyline_samples.py
@@ -1,4 +1,23 @@
 import os
+import re
+import shutil
+import site
+import sys
+import tempfile
+from pathlib import Path
+from tempfile import TemporaryDirectory
+from xml.etree.ElementTree import ParseError
+import json
+import subprocess
+
+from assemblyline_v4_service.common.base import ServiceBase
+from assemblyline_v4_service.common.request import ServiceRequest
+from assemblyline_v4_service.common.result import Result, ResultSection
+from lxml import etree
+from platformdirs import PlatformDirs
+from sandlock import Policy, Sandbox, landlock_abi_version, min_landlock_abi
+import os
+import os
 import time
 
 import pytest
@@ -19,6 +38,32 @@ th = TestHelper(service_class, RESULTS_FOLDER, SAMPLES_FOLDER)
 
 @pytest.mark.parametrize("sample", th.result_list())
 def test_sample(sample):
+    readable_files = [
+        "/lib",
+        "/lib64",
+        "/usr/lib",
+        "/usr/lib64",
+        "/bin",
+        "/usr/bin",
+        "/etc"
+    ]
+    readable_files = [os.path.realpath(r) for r in readable_files]
+
+    readable_files = list(set(readable_files))
+
+    #raise MSBuildEvalError(f"Testing readability of required paths: {readable_files}")
+
+    for r in readable_files:
+        if not os.path.exists(r):
+            print(f"Required readable path does not exist: {r}")
+
+    policy = Policy(
+        fs_readable=readable_files
+    )
+    result = Sandbox(policy).run(["/bin/true"], timeout=10)
+    print(f"PYTHON ISOLATED TEST: {result}, {result.stderr.decode()}, {result.stdout.decode()}")
+    assert result.success
+
     start_time = time.time()
     th.run_test_comparison(sample)
     print(f"Time elapsed for {sample}: {round(time.time() - start_time)}s")
